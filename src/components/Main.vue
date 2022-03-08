bc409c0cccd4aa421ec28f1bcac49b5843cc1b59
lbi <louis.bensi@bemove.com>
2022-03-08 14:50:36 +0100

    ajouts features

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,13 +8,14 @@
       </div>
 
       <div>
-        <select @change="onChange()" v-model="selected" id="location" name="location" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
-          <option value="all" selected>Choisissez la région</option>
+        <select @change="onChange()" v-model="selected" class="mt-1 block w-full pl-3 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+          <option disabled value="">Chosissez la région</option>
           <option value="africa">Afrique</option>
           <option value="americas">Amérique</option>
           <option value="asia">Asie</option>
           <option value="europe">Europe</option>
           <option value="oceania">Océanie</option>
+          <option value="all">Toutes les régions</option>
         </select>
       </div>
     </div>
@@ -31,9 +32,11 @@
         <div class="p-4 pb-8 bg-white">
           <p class="text-gray-800 font-bold">{{ country.translations.fra.common }}</p>
           <ul class="mt-4 text-sm space-y-1 text-gray-600">
-            <li><strong>Population</strong> : {{ country.population }}</li>
+            <li><strong>Population</strong> : {{ country.population }} habitants</li>
             <li><strong>Region</strong> : {{ country.region }}</li>
-            <li><strong>Capital</strong> : {{ country.capital }}</li>
+            <li v-for="cap in country.capital" :key="cap.id">
+              <strong>Capital</strong> : {{ cap }}
+            </li>
           </ul>
         </div>
       </div>
@@ -47,29 +50,43 @@ export default {
     return {
       countries: [],
       search: '',
+      selected: '',
     }
   },
+  created() {
+    this.displayCountriesAll();
+  },
   methods: {
-    onChange() {
-      let url = "https://restcountries.com/v3.1/region/" + this.selected;
+    displayCountriesAll() {
+      let url = "https://restcountries.com/v3.1/all";
       fetch(url)
           .then(res => res.json())
           .then(data => this.countries = data);
     },
+
+    // LORSQUE LE SELECT EST SELECTIONNÉ
+    onChange() {
+      if (this.selected == "all") {
+        this.displayCountriesAll()
+      } else {
+        let url = `https://restcountries.com/v3.1/region/${this.selected}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => this.countries = data);
+      }
+    },
+
+    // LORSQUE LE CHAMP INPUT EST REMPLI
     onChangeSearch() {
-      let url = "https://restcountries.com/v3.1/name/" + this.search;
+      if (this.search == "") {
+        this.displayCountriesAll();
+      }
+      let url = `https://restcountries.com/v3.1/name/${this.search}`;
       fetch(url)
           .then(res => res.json())
           .then(data => this.countries = data);
     },
   },
-  mounted() {
-    let url = "https://restcountries.com/v3.1/all";
-    fetch(url)
-        .then(res => res.json())
-        .then(data => this.countries = data);
-  }
-  ,
   name: 'Main',
 }
 </script>
