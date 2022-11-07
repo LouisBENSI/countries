47bbe52a18cc8109eefd952e60bb99421a0d6316
lbi <louis.bensi@bemove.com>
2022-11-07 11:04:40 +0100

    grid

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,23 +2,23 @@
 
   <main class="max-w-[1280px] px-4 mx-auto xl:px-0 dark:bg-gray-800">
 
-    <div class="flex flex-col justify-between mt-12 md:flex-row">
-        <input @change="onChangeSearch()" v-model="search" type="text" class="w-full h-[40px] md:max-w-[500px] border-gray-300 pl-4  rounded-md md:mr-2" placeholder="Rechercher un pays">
-        <select @change="onChange()" v-model="selected" class="mt-1 block w-full pl-3 py-2 text-base border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm md:mt-0 md:ml-2 md:w-[300px]">
-          <option disabled value="">Chosissez la région</option>
-          <option value="africa">Afrique</option>
-          <option value="americas">Amérique</option>
-          <option value="asia">Asie</option>
-          <option value="europe">Europe</option>
-          <option value="oceania">Océanie</option>
-          <option value="all">Toutes les régions</option>
-        </select>
+    <div class="flex flex-col justify-between my-12 md:flex-row">
+      <input v-model="search" type="text" class="w-full h-[40px] md:max-w-[500px] border-gray-300 pl-4  rounded-md md:mr-2" placeholder="Rechercher un pays">
+      <select v-model="selected" class="mt-1 block w-full pl-3 py-2 text-base border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm md:mt-0 md:ml-2 md:w-[300px]">
+        <option disabled value="">Chosissez la région</option>
+        <option value="africa">Afrique</option>
+        <option value="americas">Amérique</option>
+        <option value="asia">Asie</option>
+        <option value="europe">Europe</option>
+        <option value="oceania">Océanie</option>
+        <option value="all">Toutes les régions</option>
+      </select>
     </div>
 
     <!-- CONTAINER COUNTRIES -->
-    <div class="mt-8 md:mt-12 md:flex md:flex-wrap md:justify-evenly lg:justify-between">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 md:gap-8">
       <!-- COUNTRY -->
-      <div v-for="country in countries" :key="country.id" class="w-full md:w-[275px] mb-12 rounded-md overflow-hidden shadow border border-solid border-gray-200">
+      <div v-for="country in countries" :key="country.id" class="w-full rounded-md overflow-hidden shadow border border-solid border-gray-200">
         <!-- IMG -->
         <div class="bg-gray-400 h-48 w-full">
           <img :src="country.flags.svg" alt="" class="w-full h-full object-cover">
@@ -27,7 +27,7 @@
         <div class="p-4 pb-8 bg-white">
           <p class="text-gray-800 font-bold">{{ country.translations.fra.common }}</p>
           <ul class="mt-4 text-sm space-y-1 text-gray-600">
-            <li><strong>Population</strong> : {{ country.population }} habitants</li>
+            <li><strong>Population</strong> : {{ Number(country.population).toLocaleString().split(/\s/).join(', ') }} habitants</li>
             <li><strong>Region</strong> : {{ country.region }}</li>
             <li v-for="cap in country.capital" :key="cap.id">
               <strong>Capital</strong> : {{ cap }}
@@ -36,6 +36,8 @@
         </div>
       </div>
     </div>
+
+
   </main>
 </template>
 
@@ -48,40 +50,41 @@ export default {
       selected: '',
     }
   },
+
   created() {
-    this.displayCountriesAll();
+    this.displayCountriesAll()
   },
+
   methods: {
     displayCountriesAll() {
-      let url = "https://restcountries.com/v3.1/all";
+      let url = "https://restcountries.com/v3.1/all"
       fetch(url)
           .then(res => res.json())
-          .then(data => this.countries = data);
-    },
-
-    // LORSQUE LE SELECT EST SELECTIONNÉ
-    onChange() {
-      if (this.selected == "all") {
-        this.displayCountriesAll()
-      } else {
-        let url = `https://restcountries.com/v3.1/region/${this.selected}`;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => this.countries = data);
-      }
-    },
-
-    // LORSQUE LE CHAMP INPUT EST REMPLI
-    onChangeSearch() {
-      if (this.search == "") {
-        this.displayCountriesAll();
-      }
-      let url = `https://restcountries.com/v3.1/name/${this.search}`;
-      fetch(url)
-          .then(res => res.json())
-          .then(data => this.countries = data);
+          .then(data => this.countries = data)
     },
   },
+
+  watch: {
+
+    // search() {
+    //   if (this.selected === "all") {
+    //     this.displayCountriesAll()
+    //   } else {
+    //     let url = `https://restcountries.com/v3.1/region/${this.selected}`;
+    //     fetch(url)
+    //         .then(res => res.json())
+    //         .then(data => this.countries = data);
+    //   }
+    // },
+
+    selected() {
+      let url = `https://restcountries.com/v3.1/region/${this.selected}`
+      fetch(url)
+          .then(res => res.json())
+          .then(data => this.countries = data)
+    }
+  },
+
   name: 'Main',
 }
 </script>
